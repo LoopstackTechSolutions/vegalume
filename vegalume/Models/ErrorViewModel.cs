7327f09e39b570c09275ba161f00382f96e4d09b
Vinicius <vinicavequi@gmail.com>
2025-05-20 20:32:47 -0300

    commit inicial

---
+++ vegalume/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vegalume.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
